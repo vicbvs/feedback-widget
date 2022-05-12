eb0401d32c2c19e1b4df75cea3e829c716bf127b
Victor Borsatto <victor.borsattobh@gmail.com>
2022-05-11 23:05:53 -0300

    Finished the backend, creation of the feedback route, integration with prisma and unit tests with jest

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from '../mail-adapter';
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'eb2c4bd6b84360',
+    pass: 'cb5aff374ee900',
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Victor Borsatto <victor.borsattobh@gmail.com>',
+      subject,
+      html: body,
+    });
+  }
+}
